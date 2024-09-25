40dd875af64b8e7688f986482ffc84a4bdcc4d13
GerasimoskiDesign <gerasimoskiwork@gmail.com>
2024-09-25 10:30:30 +0200

    Initial Commit CardView isFaceUp

---
+++ Game/Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Game
+//
+//  Created by Mario Gerasimoski on 24.9.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Game/Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by Mario Gerasimoski on 24.9.24.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
